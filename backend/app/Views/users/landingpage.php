2399511b6d9e3a1aaf55510a108f3e350d9c7d21
AyeeYee433 <aielwrenramos@gmail.com>
2025-11-24 02:20:30 +0800

    landingpage

---
+++ backend/app/Views/users/landingpage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Landing Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <style>
+
+    </style>
+
+    <php>
+
+    </php>
+
+    <!-- Landing -->
+    <div class="land">
+        <div>
+            <h1>Game Mods</h1>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
