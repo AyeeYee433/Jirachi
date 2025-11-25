de8aa8314bd921cb50077bee094dcc9f2d0123cd
RH <aboboharvey7@gmail.com>
2025-11-25 18:01:43 +0800

    feat(auth): Updated Auth Controller with following functions and expose Routes
    Login = Auth::login
    
    Logout = Auth::logout
    
    Sign Up = Auth::signup

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -9,3 +9,9 @@ $routes->get('/', 'Users::index');
 $routes->get('/moodBoard', 'Users::moodBoard');
 $routes->get('/signUp', 'Users::signUp');
 $routes->get('/login', 'Users::login');
+
+
+
+$routes->post('/login', 'Auth::login');
+$routes->post('/signup', 'Auth::signup');
+$routes->get('/logout', 'Auth::logout');
