84f6eb818fc086bcf96c51c4dd24d1c346f266dd
Zelle <hazellemrarang@gmail.com>
2025-11-25 19:09:43 +0800

    feat(cart and checkout page): -added cart page -added checkout page -updated routes -updated controllers -updated header

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -9,3 +9,5 @@ $routes->get('/', 'Users::index');
 $routes->get('/moodBoard', 'Users::moodBoard');
 $routes->get('/signUp', 'Users::signUp');
 $routes->get('/login', 'Users::login');
+$routes->get('/cart', 'Users::cart');
+$routes->get('/checkout', 'Users::checkout');
