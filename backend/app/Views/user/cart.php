ffdb78d4aaccdfe67dfbc5ba0d048196e7ace744
RH <aboboharvey7@gmail.com>
2025-11-26 23:34:55 +0800

    refactor(backend/cartModel): Updated cart.php to show user cart dynamically based on Cart Table
    Updated Controller to send data from database

--- backend/app/Views/user/cart.php
+++ backend/app/Views/user/cart.php
@@ -1,10 +1,6 @@
 <?= view("components/head") ?>
 <?= view("components/header") ?>
 
-<?php
-$cart = session()->get('cart') ?? [];
-?>
-
 <body class="bg-gray-50">
 
     <section class="mx-auto px-6 py-16 container">
@@ -35,24 +31,24 @@ $cart = session()->get('cart') ?? [];
                     <div class="flex items-center bg-white shadow p-4 border border-gray-200 rounded-lg">
 
                         <!-- Thumbnail -->
-                        <img src="<?= esc($item['img']) ?>"
-                            alt="<?= esc($item['name']) ?>"
+                        <img src="<?= esc($item['product_img']) ?>"
+                            alt="<?= esc($item['product_name']) ?>"
                             class="border rounded-lg w-24 h-24 object-cover">
 
                         <!-- Info -->
                         <div class="flex-1 ml-4">
-                            <h3 class="font-bold text-gray-900 text-lg"><?= esc($item['name']) ?></h3>
-                            <p class="text-gray-600 text-sm">$<?= esc($item['price']) ?></p>
+                            <h3 class="font-bold text-gray-900 text-lg"><?= esc($item['product_name']) ?></h3>
+                            <p class="text-gray-600 text-sm">$<?= esc($item['product_price']) * esc($item['quantity'])?></p>
 
                             <!-- Quantity -->
                             <form method="POST" action="/update_qty" class="flex items-center gap-2 mt-3">
-                                <input type="hidden" name="name" value="<?= esc($item['name']) ?>">
+                                <input type="hidden" name="name" value="<?= esc($item['product_name']) ?>">
 
                                 <button name="action" value="minus">
                                     <?= view("components/buttons/buttonSecondary", ["text" => "-"]) ?>
                                 </button>
 
-                                <span class="px-3"><?= esc($item['qty']) ?></span>
+                                <span class="px-3 text-black"><?= esc($item['quantity']) ?></span>
 
                                 <button name="action" value="plus">
                                     <?= view("components/buttons/buttonSecondary", ["text" => "+"]) ?>
@@ -62,7 +58,7 @@ $cart = session()->get('cart') ?? [];
 
                         <!-- Remove -->
                         <form action="/remove_from_cart" method="POST" class="ml-4">
-                            <input type="hidden" name="name" value="<?= esc($item['name']) ?>">
+                            <input type="hidden" name="name" value="<?= esc($item['product_name']) ?>">
                             <button>
                                 <?= view("components/buttons/buttonPrimary", ["text" => "Remove"]) ?>
                             </button>
